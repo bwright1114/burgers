a51a5880aeda8a59a3e4a0afa0c6fa1f705c6506
Brittni Wright <bwright1114@icloud.com>
2017-05-24 13:24:25 -0400

    baby push

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000;
+var PORT = 3000;
 
 var app = express();
 
@@ -19,7 +19,6 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
-app.use("/", routes);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
